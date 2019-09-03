0a5bbf7f20f7997c060dae20922ab563d86c1003
SarahW <pcem@pcem-emulator.co.uk>
2019-09-03 20:13:19 +0100

    Updates to podule API documentation.

--- src/podule_api.h
+++ src/podule_api.h
@@ -17,22 +17,74 @@ typedef enum podule_io_type
 
 typedef struct podule_functions_t
 {
-        /*Initialise podule instance. Any instance data should be stored
-          in podule->p
-          Returns zero on success, non-zero on error.*/
+        /*init() - Initialise podule instance
+          @podule: podule pointer
+          
+          Returns zero on success, non-zero on error.
+
+          Any instance data should be stored in podule->p*/
         int (*init)(struct podule_t *podule);
-        /*Close podule instance, and free any instance data*/
+        /*close() - Close podule instance
+          @podule: podule pointer
+          
+          This function is responsible for freeing any instance data in podule->p*/
         void (*close)(struct podule_t *podule);
 
+        /*write_b() - Write byte to podule space
+          @podule: podule pointer
+          @type:   type of access (see @podule_io_type)
+          @addr:   address to write to
+          @val:    value to write*/
         void (*write_b)(struct podule_t *podule, podule_io_type type, uint32_t addr, uint8_t val);
+        /*write_w() - Write word to podule space
+          @podule: podule pointer
+          @type:   type of access (see @podule_io_type)
+          @addr:   address to write to
+          @val:    value to write*/
         void (*write_w)(struct podule_t *podule, podule_io_type type, uint32_t addr, uint16_t val);
+        /*write_l() - Write long word to podule space
+          @podule: podule pointer
+          @type:   type of access (see @podule_io_type)
+          @addr:   address to write to
+          @val:    value to write*/
         void (*write_l)(struct podule_t *podule, podule_io_type type, uint32_t addr, uint32_t val);
+        /*read_b() - Read byte from podule space
+          @podule: podule pointer
+          @type:   type of access (see @podule_io_type)
+          @addr:   address to read from
+
+          Returns value read from podule space*/
         uint8_t  (*read_b)(struct podule_t *podule, podule_io_type type, uint32_t addr);
+        /*read_w() - Read word from podule space
+          @podule: podule pointer
+          @type:   type of access (see @podule_io_type)
+          @addr:   address to read from
+
+          Returns value read from podule space*/
         uint16_t (*read_w)(struct podule_t *podule, podule_io_type type, uint32_t addr);
+        /*read_l() - Read long from podule space
+          @podule: podule pointer
+          @type:   type of access (see @podule_io_type)
+          @addr:   address to read from
+
+          Returns value read from podule space*/
         uint32_t (*read_l)(struct podule_t *podule, podule_io_type type, uint32_t addr);
 
+        /*reset() - Reset podule
+          @podule: podule pointer*/
         void (*reset)(struct podule_t *podule);
 
+        /*run() - Callback from podule timer
+          @podule:       podule pointer
+          @timeslice_us: time expired since last run() call
+          
+          Returns delay (in us) to reload timer with. Non-zero delay can be used
+          to create a free-running timer. Returning zero will stop the timer.
+          
+          When using a free-running timer, @timeslice_us must be used to check
+          how much time has actually passed since the last callback. Emulators
+          should support at least a 1ms timer granularity. Arculator will
+          generally implement a one instruction granularity*/
         int (*run)(struct podule_t *podule, int timeslice_us);
 } podule_functions_t;
 
@@ -136,7 +188,9 @@ typedef struct podule_header_t
 
 typedef struct podule_t
 {
+        /*Pointer to header for this podule*/
         const podule_header_t *header;
+        /*Pointer to private instance data for this podule*/
         void *p;
 } podule_t;
 
@@ -145,18 +199,53 @@ typedef struct podule_t
 
 typedef struct podule_callbacks_t
 {
-        /*Update IRQ state*/
+        /*set_irq() - Update IRQ state
+          @podule: podule pointer
+          @state:  new IRQ state*/
         void (*set_irq)(podule_t *podule, int state);
-        /*Update FIQ state*/
+        /*set_fiq() - Update FIQ state
+          @podule: podule pointer
+          @state:  new FIQ state*/
         void (*set_fiq)(podule_t *podule, int state);
 
+        /*set_timer_delay_us() - Set timer delay until podule run() function called
+          @podule:   podule pointer
+          @delay_us: desired delay in us*/
         void (*set_timer_delay_us)(podule_t *podule, int delay_us);
+        /*get_timer_remaining_us() - Return time until timer expires and run()
+                                     is called
+          @podule: podule pointer
+
+          Returns: Time until timer expires in us, or zero if timer already
+          expired*/
         int (*get_timer_remaining_us)(podule_t *podule);
+        /*stop_timer() - Stop timer from running. run() will not be called
+          @podule: podule pointer*/
         void (*stop_timer)(podule_t *podule);
-        
+
+        /*config_get_int() - Read integer variable from podule configuration
+          @podule: podule pointer
+          @name:   name of variable
+          @def:    default value to return if variable does not exist
+          
+          Returns: Value of variable, or @def if variable does not exist*/
         int (*config_get_int)(podule_t *podule, const char *name, int def);
+        /*config_get_string() - Read string variable from podule configuration
+          @podule: podule pointer
+          @name:   name of variable
+          @def:    default string to return if variable does not exist
+
+          Returns: Pointer to variable string, or @def if variable does not exist*/
         const char *(*config_get_string)(podule_t *podule, const char *name, const char *def);
+        /*config_set_int() - Write integer variable to podule configuration
+          @podule: podule pointer
+          @name:   name of variable
+          @val:    value to write*/
         void (*config_set_int)(podule_t *podule, const char *name, int val);
+        /*config_set_string() - Write string variable to podule configuration
+          @podule: podule pointer
+          @name:   name of variable
+          @val:    Pointer to string to write*/
         void (*config_set_string)(podule_t *podule, const char *name, char *val);
         
         /*config_open() - Open new configuration dialogue.

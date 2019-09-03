ada5525a82725d6c5e5b0c6142a581c00aad7a00
SarahW <pcem@pcem-emulator.co.uk>
2019-09-03 11:38:06 +0100

    Add missing resetmouse() prototype in keyboard.h.

--- src/keyboard.h
+++ src/keyboard.h
@@ -6,6 +6,7 @@ void keyboard_write(uint8_t val);
 void key_do_rx_callback();
 void key_do_tx_callback();
 
+void resetmouse();
 void doosmouse();
 void setmousepos(uint32_t a);
 void getunbufmouse(uint32_t a);
